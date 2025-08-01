9a9ba6ed4b83cf457a1e1671391f7d18bfef51c6
CaioDPires <caiopires@usp.br>
2025-08-01 18:22:58 -0300

    Tela carregando (embora muito feita)

--- recipe-app/app/_layout.tsx
+++ recipe-app/app/_layout.tsx
@@ -1,5 +1,22 @@
+import { appTheme } from "@/src/themes/theme";
+import { ThemeProvider } from "@/src/themes/ThemeContext";
 import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider>
+      <SafeAreaView style={styles.container}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </ThemeProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: appTheme.background,
+  }
+})
---
+++ recipe-app/src/themes/theme.ts
@@ -0,0 +1,8 @@
+export const appTheme = {
+  background: "#FFFFFF",
+  text: "#111827",
+  primary: "#2563EB",
+};
+
+
+export type Theme = typeof appTheme;
